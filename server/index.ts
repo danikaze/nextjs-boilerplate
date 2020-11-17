e4f0617bf1a418fe281db8b39a6f15bc6dec0944
danikaze <danikaze@gmail.com>
2020-11-17 21:15:44 +0900

    server requires files from parent folders and compilation routes change depending on production/develop modes

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,22 @@
-const { getConstants } = require('../build-tools/build-time-constants');
+/**
+ * Because the custom server doesn't accept webpack
+ * due to the tsc compilation, routes are different in development/production
+ * Later, PROJECT_ROOT can be used but this is done before it's available
+ */
+const getConstants = (() => {
+  const file =
+    process.env.NODE_ENV !== 'production'
+      ? '../build-tools/build-time-constants'
+      : '../../build-tools/build-time-constants';
+
+  return require(file).getConstants as (options: {
+    type: 'custom-server' | 'server' | 'client';
+    dev: boolean;
+    isServer: boolean;
+    buildId?: string;
+  }) => { [key: string]: unknown };
+})();
+
 /*
  * Because server code doesn't work with webpack
  * set the build-time constants into the global context
