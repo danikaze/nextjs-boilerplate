0d4db41774181c34804ec4a91f58504dd15e71f8
danikaze <danikaze@gmail.com>
2021-05-03 12:28:56 +0900

    Use user.userId instead of user.id

--- utils/__test/auth.spec.ts
+++ utils/__test/auth.spec.ts
@@ -1,145 +1,369 @@
 import 'jest';
-import { GetServerSidePropsContext } from 'next';
 import sinon from 'sinon';
-import { Request, Response, NextFunction } from 'express';
+import { NextApiResponse } from 'next';
+import { ApiRequest, HttpStatus } from '@api';
 import { UserAuthData, UserRole } from '@model/user';
 import { resetAllGlobalValues, setGlobalValue } from '@test/set-global-value';
-import { adminRequired, logoutRequired, userRequired } from '../auth';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+} from '../../pages/_app';
+import {
+  userRequiredServerSideProps,
+  adminRequiredServerSideProps,
+  logoutRequiredServerSideProps,
+  userRequiredApiHandler,
+  adminRequiredApiHandler,
+  isUser,
+  isAdmin,
+} from '../auth';
 
-type ParamShaper = typeof getCtxParams | typeof getMiddlewareParams;
+// tslint:disable-next-line: no-any
+type GenericRequest = ApiRequest<any, any>;
+
+describe('isUser', () => {
+  it('should return false for non-logged users', () => {
+    const user = getUserData(false);
+    expect(isUser(user)).toBeFalsy();
+  });
+
+  it('should return false for non-expected role users', () => {
+    const user = getUserData('wrong-role');
+    expect(isUser(user)).toBeFalsy();
+  });
+
+  it('should return false for users', () => {
+    const user = getUserData('user');
+    expect(isUser(user)).toBeTruthy();
+  });
+
+  it('should return false for admins', () => {
+    const user = getUserData('admin');
+    expect(isUser(user)).toBeTruthy();
+  });
+});
+
+describe('isAdmin', () => {
+  it('should return false for non-logged users', () => {
+    const user = getUserData(false);
+    expect(isAdmin(user)).toBeFalsy();
+  });
+
+  it('should return false for non-expected role users', () => {
+    const user = getUserData('wrong-role');
+    expect(isAdmin(user)).toBeFalsy();
+  });
+
+  it('should return false for users', () => {
+    const user = getUserData('user');
+    expect(isAdmin(user)).toBeFalsy();
+  });
+
+  it('should return false for admins', () => {
+    const user = getUserData('admin');
+    expect(isAdmin(user)).toBeTruthy();
+  });
+});
+
+describe('userRequiredServerSideProps', () => {
+  afterEach(() => {
+    resetAllGlobalValues();
+  });
+
+  it('should redirect access of non-logged users', async () => {
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData(false);
+    const getServerSideProps = userRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    expect(spy.res.redirect.firstCall.firstArg).toMatch(AUTH_LOGIN_PAGE);
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should redirect access of non-expected role users when AUTH_FORBIDDEN_PAGE is set', async () => {
+    const forbiddenPage = '/forbidden';
+    setGlobalValue('AUTH_FORBIDDEN_PAGE', forbiddenPage);
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData('wrong-role');
+    const getServerSideProps = userRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    expect(spy.res.redirect.firstCall.firstArg).toMatch(AUTH_FORBIDDEN_PAGE);
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should just send HTTP_FORBIDDEN when AUTH_FORBIDDEN_PAGE is not set', async () => {
+    const forbiddenPage = undefined;
+    setGlobalValue('AUTH_FORBIDDEN_PAGE', forbiddenPage);
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData('wrong-role');
+    const getServerSideProps = userRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    expect(spy.res.sendStatus.firstCall.firstArg).toBe(
+      HttpStatus.HTTP_FORBIDDEN
+    );
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should just send HTTP_FORBIDDEN without failing if AUTH_FORBIDDEN_PAGE is set to an invalid url', async () => {
+    const invalidPage = 123;
+    setGlobalValue('AUTH_FORBIDDEN_PAGE', invalidPage);
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData('wrong-role');
+    const getServerSideProps = userRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    expect(spy.res.sendStatus.firstCall.firstArg).toBe(
+      HttpStatus.HTTP_FORBIDDEN
+    );
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should allow access of logged users', async () => {
+    const { cutProps, userProps, userGSSP, spy, ctx } = getTestData('user');
+    const getServerSideProps = userRequiredServerSideProps(cutProps, userGSSP);
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(userProps);
+    expect(spy.res.redirect.called).toBeFalsy();
+    expect(userGSSP.called).toBeTruthy();
+  });
+
+  it('should allow access of admin users', async () => {
+    const { cutProps, userProps, userGSSP, spy, ctx } = getTestData('admin');
+    const getServerSideProps = userRequiredServerSideProps(cutProps, userGSSP);
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(userProps);
+    expect(spy.res.redirect.called).toBeFalsy();
+    expect(userGSSP.called).toBeTruthy();
+  });
+});
+
+describe('adminRequiredServerSideProps', () => {
+  it('should redirect access of non-logged users', async () => {
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData(false);
+    const getServerSideProps = adminRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    expect(spy.res.redirect.firstCall.firstArg).toMatch(AUTH_LOGIN_PAGE);
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should redirect access of non-expected role users', async () => {
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData('wrong-role');
+    const getServerSideProps = adminRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    if (AUTH_FORBIDDEN_PAGE) {
+      expect(spy.res.redirect.firstCall.firstArg).toMatch(AUTH_FORBIDDEN_PAGE);
+    } else {
+      expect(spy.res.sendStatus.firstCall.firstArg).toBe(
+        HttpStatus.HTTP_FORBIDDEN
+      );
+    }
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should not allow access of simple users', async () => {
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData('user');
+    const getServerSideProps = adminRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    if (AUTH_FORBIDDEN_PAGE) {
+      expect(spy.res.redirect.firstCall.firstArg).toMatch(AUTH_FORBIDDEN_PAGE);
+    } else {
+      expect(spy.res.sendStatus.firstCall.firstArg).toBe(
+        HttpStatus.HTTP_FORBIDDEN
+      );
+    }
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should allow access of admin users', async () => {
+    const { cutProps, userProps, userGSSP, spy, ctx } = getTestData('admin');
+    const getServerSideProps = adminRequiredServerSideProps(cutProps, userGSSP);
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(userProps);
+    expect(spy.res.redirect.called).toBeFalsy();
+    expect(userGSSP.called).toBeTruthy();
+  });
+});
+
+describe('logoutRequiredServerSideProps', () => {
+  it('should logout users', async () => {
+    const { cutProps, notCalledGSSP, spy, ctx } = getTestData('user');
+    const getServerSideProps = logoutRequiredServerSideProps(
+      cutProps,
+      notCalledGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(cutProps);
+    expect(spy.res.redirect.called).toBeTruthy();
+    expect(spy.res.redirect.firstCall.firstArg).toMatch(AUTH_DO_LOGOUT_URL);
+    expect(notCalledGSSP.called).toBeFalsy();
+  });
+
+  it('should do nothing for already non-logged in users', async () => {
+    const { cutProps, noUserProps, noUserGSSP, spy, ctx } = getTestData(false);
+    const getServerSideProps = logoutRequiredServerSideProps(
+      cutProps,
+      noUserGSSP
+    );
+    const result = await getServerSideProps(ctx);
+    expect(result.props).toBe(noUserProps);
+    expect(spy.res.redirect.called).toBeFalsy();
+    expect(noUserGSSP.called).toBeTruthy();
+  });
+});
+
+describe('userRequiredApiHandler', () => {
+  it('should return 401 error when a non-logged users tries to access', () => {
+    const { spy, req, res, notCalledApiHandler } = getTestData(false);
+    const apiHandler = userRequiredApiHandler(notCalledApiHandler);
+    apiHandler(req, res);
+    expect(notCalledApiHandler.called).toBeFalsy();
+    expect(spy.res.status.firstCall.firstArg).toBe(HttpStatus.HTTP_FORBIDDEN);
+    expect(spy.res.end.called).toBeTruthy();
+  });
+
+  it('should return 401 error when a user with wrong role tries to access', () => {
+    const { spy, req, res, notCalledApiHandler } = getTestData('wrong-role');
+    const apiHandler = userRequiredApiHandler(notCalledApiHandler);
+    apiHandler(req, res);
+    expect(notCalledApiHandler.called).toBeFalsy();
+    expect(spy.res.status.firstCall.firstArg).toBe(HttpStatus.HTTP_FORBIDDEN);
+    expect(spy.res.end.called).toBeTruthy();
+  });
+
+  it('should allow access when an authenticated user accesses', () => {
+    const { spy, req, res, authApiHandler } = getTestData('user');
+    const apiHandler = userRequiredApiHandler(authApiHandler);
+    apiHandler(req, res);
+    expect(authApiHandler.called).toBeTruthy();
+    expect(authApiHandler.firstCall.args[0]).toBe(req);
+    expect(authApiHandler.firstCall.args[1]).toBe(res);
+    expect(spy.res.status.called).toBeFalsy();
+  });
+
+  it('should allow access when an admin user accesses', () => {
+    const { spy, req, res, authApiHandler } = getTestData('admin');
+    const apiHandler = userRequiredApiHandler(authApiHandler);
+    apiHandler(req, res);
+    expect(authApiHandler.called).toBeTruthy();
+    expect(authApiHandler.firstCall.args[0]).toBe(req);
+    expect(authApiHandler.firstCall.args[1]).toBe(res);
+    expect(spy.res.status.called).toBeFalsy();
+  });
+});
+
+describe('adminRequiredApiHandler', () => {
+  it('should return 401 error when a non-logged users tries to access', () => {
+    const { spy, req, res, notCalledApiHandler } = getTestData(false);
+    const apiHandler = adminRequiredApiHandler(notCalledApiHandler);
+    apiHandler(req, res);
+    expect(notCalledApiHandler.called).toBeFalsy();
+    expect(spy.res.status.firstCall.firstArg).toBe(HttpStatus.HTTP_FORBIDDEN);
+    expect(spy.res.end.called).toBeTruthy();
+  });
+
+  it('should return 401 error when a user with wrong role tries to access', () => {
+    const { spy, req, res, notCalledApiHandler } = getTestData('wrong-role');
+    const apiHandler = adminRequiredApiHandler(notCalledApiHandler);
+    apiHandler(req, res);
+    expect(notCalledApiHandler.called).toBeFalsy();
+    expect(spy.res.status.firstCall.firstArg).toBe(HttpStatus.HTTP_FORBIDDEN);
+    expect(spy.res.end.called).toBeTruthy();
+  });
+
+  it('should  return 401 error when an basic user accesses', () => {
+    const { spy, req, res, notCalledApiHandler } = getTestData('user');
+    const apiHandler = adminRequiredApiHandler(notCalledApiHandler);
+    apiHandler(req, res);
+    expect(notCalledApiHandler.called).toBeFalsy();
+    expect(spy.res.status.firstCall.firstArg).toBe(HttpStatus.HTTP_FORBIDDEN);
+    expect(spy.res.end.called).toBeTruthy();
+  });
+
+  it('should allow access when an admin user accesses', () => {
+    const { spy, req, res, authApiHandler } = getTestData('admin');
+    const apiHandler = adminRequiredApiHandler(authApiHandler);
+    apiHandler(req, res);
+    expect(authApiHandler.called).toBeTruthy();
+    expect(authApiHandler.firstCall.args[0]).toBe(req);
+    expect(authApiHandler.firstCall.args[1]).toBe(res);
+    expect(spy.res.status.called).toBeFalsy();
+  });
+});
 
 /*
- * runTest is just a way to define the same test cases and run them twice
- * with different parameters but without code duplication
+ * Test helpers
  */
-function runTest(desc: string, paramShaper: ParamShaper) {
-  // shapeParams is just a wrapper so paramShaper returns the correct parameters
-  const shapeParams = paramShaper as typeof getCtxParams;
-  const HTTP_FORBIDDEN = 401;
-
-  describe(`userRequired (${desc})`, () => {
-    it('should allow accesses of logged users', () => {
-      const data = getTestData('user');
-      const params = shapeParams(data);
-      const res = userRequired(...params);
-      expect(res).toBeUndefined();
-      expect(data.res.redirect.called).toBeFalsy();
-    });
-
-    it('should allow accesses of logged admins', () => {
-      const data = getTestData('admin');
-      const params = shapeParams(data);
-      const res = userRequired(...params);
-      expect(res).toBeUndefined();
-      expect(data.res.redirect.called).toBeFalsy();
-    });
-
-    it('should redirect access of non-logged users', () => {
-      const data = getTestData(false);
-      const params = shapeParams(data);
-      const res = userRequired(...params);
-      expect(res).toBe(true);
-      expect(data.res.redirect.called).toBeTruthy();
-      expect(data.res.redirect.firstCall.firstArg).toMatch(AUTH_LOGIN_PAGE);
-    });
-  });
-
-  describe(`adminRequired (${desc})`, () => {
-    afterEach(() => {
-      resetAllGlobalValues();
-    });
-
-    it('should redirect access of logged users but without the correct role, when AUTH_FORBIDDEN_PAGE is set', () => {
-      const forbidenPage = '/forbidden';
-      setGlobalValue('AUTH_FORBIDDEN_PAGE', forbidenPage);
-
-      const data = getTestData('user');
-      const params = shapeParams(data);
-      const res = adminRequired(...params);
-      expect(res).toBe(true);
-      expect(data.res.redirect.called).toBeTruthy();
-      expect(data.res.redirect.firstCall.firstArg).toBe(forbidenPage);
-      expect(data.res.sendStatus.called).toBeFalsy();
-    });
-
-    it('should send 401 for logged users but without the correct role, when AUTH_FORBIDDEN_PAGE is not set', () => {
-      setGlobalValue('AUTH_FORBIDDEN_PAGE', undefined);
-
-      const data = getTestData('user');
-      const params = shapeParams(data);
-      const res = adminRequired(...params);
-      expect(res).toBe(true);
-      expect(data.res.redirect.called).toBeFalsy();
-      expect(data.res.sendStatus.called).toBeTruthy();
-      expect(data.res.sendStatus.firstCall.firstArg).toBe(HTTP_FORBIDDEN);
-    });
-
-    it('should send 401 for logged users but without the correct role, when AUTH_FORBIDDEN_PAGE is set but empty', () => {
-      setGlobalValue('AUTH_FORBIDDEN_PAGE', '');
-
-      const data = getTestData('user');
-      const params = shapeParams(data);
-      const res = adminRequired(...params);
-      expect(res).toBe(true);
-      expect(data.res.redirect.called).toBeFalsy();
-      expect(data.res.sendStatus.called).toBeTruthy();
-      expect(data.res.sendStatus.firstCall.firstArg).toBe(HTTP_FORBIDDEN);
-    });
-
-    it('should send 401 for logged users but without the correct role, when AUTH_FORBIDDEN_PAGE is set to an invalid value', () => {
-      setGlobalValue('AUTH_FORBIDDEN_PAGE', []);
-
-      const data = getTestData('user');
-      const params = shapeParams(data);
-      const res = adminRequired(...params);
-      expect(res).toBe(true);
-      expect(data.res.redirect.called).toBeTruthy();
-      // ^ redirect is called but with wrong parameter
-      expect(data.res.sendStatus.called).toBeTruthy();
-      expect(data.res.sendStatus.firstCall.firstArg).toBe(HTTP_FORBIDDEN);
-    });
-  });
-
-  describe(`logoutRequired (${desc})`, () => {
-    it('should logout users', () => {
-      const data = getTestData('user');
-      const params = shapeParams(data);
-      const res = logoutRequired(...params);
-      expect(res).toBe(true);
-      expect(data.res.redirect.called).toBeTruthy();
-      expect(data.res.redirect.firstCall.firstArg).toMatch(AUTH_DO_LOGOUT_URL);
-    });
-
-    it('should do nothing for already logged out users', () => {
-      const data = getTestData(false);
-      const params = shapeParams(data);
-      const res = logoutRequired(...params);
-      expect(res).toBeUndefined;
-      expect(data.res.redirect.called).toBeFalsy();
-    });
-  });
+function getUserData(role: string | false): UserAuthData | false {
+  return (
+    (role && {
+      role: role as UserRole,
+      username: 'username',
+      userId: 1,
+    }) ||
+    false
+  );
 }
 
-runTest('ctx', getCtxParams);
-runTest('express middleware', getMiddlewareParams);
+function getTestData(role: string | false, originalUrl: string = 'url') {
+  const cutProps = { called: false };
+  const userProps = { user: true };
+  const noUserProps = { user: false };
 
-/**
- * Prepares data for the test
- */
-function getTestData(role: UserRole | false, originalUrl: string = 'url') {
-  const user: UserAuthData | false =
-    (role && {
-      role,
-      username: 'username',
-      id: 1,
-    }) ||
-    false;
+  const notCalledGSSP = sinon.spy((async () => ({
+    props: {},
+  })) as GetServerSideProps);
+
+  const userGSSP = sinon.spy((async () => ({
+    props: userProps,
+  })) as GetServerSideProps);
+
+  const noUserGSSP = sinon.spy((async () => ({
+    props: noUserProps,
+  })) as GetServerSideProps);
+
+  const authApiHandler = sinon.spy((req, res) => {
+    expect(req.user).toBeTruthy();
+  });
+
+  const notCalledApiHandler = sinon.spy(() => {
+    expect(true).toBeFalsy();
+  });
+
+  const user = getUserData(role);
   const req = { originalUrl, user };
   const res = {
     redirect: sinon.stub(),
+    status: sinon.stub(),
+    json: sinon.stub(),
     sendStatus: sinon.stub(),
     end: sinon.stub(),
   };
+  const ctx = ({
+    req,
+    res,
+  } as unknown) as GetServerSidePropsContext;
 
   res.redirect.callsFake((url: unknown) => {
     // simulate an exception when url is invalid
@@ -149,32 +373,17 @@ function getTestData(role: UserRole | false, originalUrl: string = 'url') {
   });
 
   return {
-    req,
-    res,
+    cutProps,
+    userProps,
+    noUserProps,
+    ctx,
+    notCalledGSSP,
+    userGSSP,
+    noUserGSSP,
+    authApiHandler,
+    notCalledApiHandler,
+    req: (req as unknown) as GenericRequest,
+    res: (res as unknown) as NextApiResponse,
+    spy: { res },
   };
-}
-
-/**
- * Shapes the test data as Ctx-like parameters
- */
-function getCtxParams({
-  req,
-  res,
-}: ReturnType<typeof getTestData>): [GetServerSidePropsContext] {
-  return [
-    ({
-      req,
-      res,
-    } as unknown) as GetServerSidePropsContext,
-  ];
-}
-
-/**
- * Shapes the test data as parameters for a express middleware
- */
-function getMiddlewareParams({
-  req,
-  res,
-}: ReturnType<typeof getTestData>): [Request, Response, NextFunction] {
-  return [(req as unknown) as Request, (res as unknown) as Response, () => {}];
 }
