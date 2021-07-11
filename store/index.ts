bc3075cd5abc260fed0cf28451809440cd693473
danikaze <danikaze@gmail.com>
2021-07-11 14:00:31 +0900

    Update version of next-redux-wrapper to 7.x.x

--- store/index.ts
+++ store/index.ts
@@ -1,29 +1,29 @@
-import { applyMiddleware, createStore } from 'redux';
+import { GetStaticProps, NextComponentType, NextPageContext } from 'next';
 import { createWrapper, MakeStore } from 'next-redux-wrapper';
+import { applyMiddleware, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware, {
   ThunkDispatch as ReduxThunkDispatch,
 } from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import reduxInmutableStateInvariant from 'redux-immutable-state-invariant';
+import { GetServerSideProps } from '@_app';
 import { State } from './model';
 import { Action } from './actions';
 import { reducer } from './reducers';
-import { UserAuthData } from '@model/user';
 
+// tslint:disable: no-any
 export type ThunkDispatch<A extends Action> = ReduxThunkDispatch<
   State,
   null,
   A
 >;
-// tslint:disable-next-line: no-any
 export type ActionCreator<A extends Action> = (...args: any[]) => A;
-// tslint:disable-next-line: no-any
 export type ThunkActionCreator<A extends Action, T extends any[] = any[]> = (
   ...args: T
 ) => (dispatch: ThunkDispatch<A>, getState: () => State) => void;
 
-const makeStore: MakeStore<State, Action> = (context) => {
+const makeStore: MakeStore<Store<State, Action>> = (context) => {
   const middleware = [thunkMiddleware, promiseMiddleware];
 
   if (!IS_PRODUCTION) {
@@ -36,24 +36,32 @@ const makeStore: MakeStore<State, Action> = (context) => {
   );
 };
 
-const w = createWrapper<State, Action>(makeStore, {
+const w = createWrapper<Store<State, Action>>(makeStore, {
   debug: !IS_PRODUCTION,
 });
 
-type Gssp = typeof w['getServerSideProps'];
-type Ctx = Parameters<Parameters<Gssp>[0]>[0];
-type Store = Omit<typeof w, 'getServerSideProps'> & {
-  // tslint:disable-next-line:no-any
+type GetInitialPageProps<P> = NextComponentType<
+  NextPageContext,
+  any,
+  P
+>['getInitialProps'];
+
+type AppStore = Omit<
+  typeof w,
+  'getServerSideProps' | 'getStaticProps' | 'getInitialPageProps'
+> & {
   getServerSideProps: <P extends {} = any>(
-    callback: (
-      ctx: Ctx & {
-        req: { user: UserAuthData | false };
-      }
-    ) => void | P
-  ) => ReturnType<Gssp>;
+    callback: (store: Store<State, Action>) => GetServerSideProps<P>
+  ) => void;
+  getStaticProps: <P extends {} = any>(
+    callback: (store: Store<State, Action>) => GetStaticProps<P>
+  ) => void;
+  getInitialPageProps: <P extends {} = any>(
+    callback: (store: Store<State, Action>) => GetInitialPageProps<P>
+  ) => void;
 };
 
 // `wrapper` is required to be named like this
 // but the alias `store` is provided as well to make the code clearer
-export const wrapper = w as Store;
-export const store = w as Store;
+export const wrapper = (w as unknown) as AppStore;
+export const store = (w as unknown) as AppStore;
